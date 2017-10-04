66f1aea8ad802c1c83c9d4cca85afad16588ba8d
Kristina Del Rio Albrechet <kalbrechet@gmail.com>
2017-10-04 22:43:28 +0300

    Added winnerdata class

---
+++ MemoryTrackerObj/Model/WinnerData.h
@@ -0,0 +1,24 @@
+//
+//  WinnerData.h
+//  MemoryTrackerObj
+//
+//  Created by Alejandro Del Rio Albrechet on 10/4/17.
+//  Copyright © 2017 Kristina Del Rio Albrechet. All rights reserved.
+//
+
+#ifndef WinnerData_h
+#define WinnerData_h
+
+#import <Foundation/Foundation.h>
+
+@interface WinnerData: NSObject<NSCoding>
+
+@property NSString* username;
+@property NSNumber* score;
+@property NSDate* winDate;
+
+- (id) init;
+
+@end
+
+#endif /* WinnerData_h */
---
+++ MemoryTrackerObj/Model/WinnerData.m
@@ -0,0 +1,42 @@
+//
+//  WinnerData.m
+//  MemoryTrackerObj
+//
+//  Created by Alejandro Del Rio Albrechet on 10/4/17.
+//  Copyright © 2017 Kristina Del Rio Albrechet. All rights reserved.
+//
+
+#import "WinnerData.h"
+
+@implementation WinnerData
+
+@synthesize username;
+@synthesize score;
+@synthesize winDate;
+
+- (id) init {
+    self = [super init];
+    
+    username = @"Unknown";
+    score = 0;
+    winDate = [NSDate date];
+    
+    return self;
+}
+
+- (id) initWithCoder: (NSCoder*) decoder {
+    self = [super init];
+    
+    username = [decoder decodeObjectForKey:@"username"];
+    score = [decoder    decodeObjectForKey:@"score"];
+    winDate = [decoder  decodeObjectForKey:@"date"];
+    
+    return self;
+}
+- (void) encodeWithCoder: (NSCoder*) encoder {
+    [encoder encodeObject:username forKey:@"username"];
+    [encoder encodeObject:score    forKey:@"score"];
+    [encoder encodeObject:winDate  forKey:@"date"];
+}
+
+@end
