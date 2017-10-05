18958e701c9f4dcfd9e21f2f55350cd14ac0c5db
Kristina Del Rio Albrechet <kalbrechet@gmail.com>
2017-10-05 23:34:12 +0300

    Added extension for NSDate

---
+++ MemoryTrackerObj/Extensions/NSDate+TimeSinceDate.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+TimeSinceDate.h
+//  MemoryTrackerObj
+//
+//  Created by Alejandro Del Rio Albrechet on 10/5/17.
+//  Copyright © 2017 Kristina Del Rio Albrechet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (TimeSinceDate)
+
+- (NSString *)timeSinceDate;
+
+@end
---
+++ MemoryTrackerObj/Extensions/NSDate+TimeSinceDate.m
@@ -0,0 +1,59 @@
+//
+//  NSDate+TimeSinceDate.m
+//  MemoryTrackerObj
+//
+//  Created by Alejandro Del Rio Albrechet on 10/5/17.
+//  Copyright © 2017 Kristina Del Rio Albrechet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSDate+TimeSinceDate.h"
+
+@implementation NSDate (TimeSinceDate)
+
+- (NSString *)timeSinceDate {
+    NSString *durationString;
+    
+    NSDate *now = [NSDate date];
+    NSDate *startDate = [now earlierDate:self];
+    NSDate *endDate = (startDate == now) ? self : now;
+    
+    NSDateComponents *components = [[NSCalendar currentCalendar]
+                                    components: NSCalendarUnitDay|NSCalendarUnitHour|NSCalendarUnitMinute fromDate: startDate toDate: endDate options: 0];
+    
+    NSArray *timeUnits = [[NSArray alloc] initWithObjects:
+                    [[NSNumber alloc] initWithInteger:[components weekOfYear]],
+                    [[NSNumber alloc] initWithInteger:[components day]],
+                    [[NSNumber alloc] initWithInteger:[components hour]],
+                    [[NSNumber alloc] initWithInteger:[components minute]],
+                    [[NSNumber alloc] initWithInteger:[components second]], nil];
+    
+    
+    durationString = [self createDurationStringWith:timeUnits andStrAnalog:@"week"];
+
+
+    return durationString ? durationString : @"";
+}
+
+- (NSString *)createDurationStringWith:(NSArray *)timeUnits andStrAnalog: (NSString *)insertor {
+    NSString *durationString = [NSString alloc];
+    NSInteger ind = 0;
+    while (ind < [timeUnits count] && !durationString) {
+        NSInteger timeUnit = [[timeUnits objectAtIndex:ind++] integerValue];
+        if (timeUnit > 0) {
+            if(timeUnit > 1) {
+                durationString = [durationString initWithFormat: @"%li %@ s ago", (long)timeUnit, insertor];
+                break;
+            } else {
+                durationString = [durationString initWithFormat: @"%li %@ ago", (long)timeUnit, insertor];
+                break;
+            }
+        }
+    }
+    
+    return durationString;
+}
+
+@end
+
+
